71f9150c6d4e162ab25dad7745e02c031d50a1d5
Sebastian "Seabass" Cruz <haloguy1175@gmail.com>
2025-01-29 12:54:48 -0800

    fixed subfolders

--- src/helpers/images.ts
+++ src/helpers/images.ts
@@ -1,22 +1,38 @@
-import tattoovertical from '../assets/Personal-Website-Gif.gif'
-import tattoohorizontal from '../assets/Personal-Website-Gif-Sideways.gif'
-import tvmask from '../assets/Personal-Website-TV.png'
-import tvbackground from '../assets/tv-background.png'
-import tvdefault from '../assets/tempdefault.png'
-import keyboardwarrior from '../assets/KeyboardWarriorImage.gif'
-import quickcast from '../assets/QuickcastImage.gif'
-import blendify from '../assets/BlendifyImage.gif'
-import photography from '../assets/PhotographyImage.gif'
-import nishikigoi from '../assets/tempdefault.png'
-import graphicdesign from '../assets/tempdefault.png'
-import keyboards from '../assets/tempdefault.png'
-import nametitle from '../assets/streamText.gif'
-import about1 from '../assets/DSCF0789.jpg'
-import about2 from '../assets/DSCF1339.jpg'
-import about3 from '../assets/DSCF1474.jpg'
-import blendifyposter from '../assets/Blendify-Poster-Board.png'
-import blendifyblack from '../assets/Blendify Logo.png'
-import blendifypurple from '../assets/Blendify Logo Purple.png'
+import tattoovertical from '../assets/Homepage/Personal-Website-Gif.gif'
+import tattoohorizontal from '../assets/Homepage/Personal-Website-Gif-Sideways.gif'
+import tvmask from '../assets/Homepage/Personal-Website-TV.png'
+import tvbackground from '../assets/Homepage/tv-background.png'
+import tvdefault from '../assets/Homepage/tempdefault.png'
+import keyboardwarrior from '../assets/Homepage/KeyboardWarriorImage.gif'
+import quickcast from '../assets/Homepage/QuickcastImage.gif'
+import blendify from '../assets/Homepage/BlendifyImage.gif'
+import photography from '../assets/Homepage/PhotographyImage.gif'
+import nishikigoi from '../assets/Homepage/tempdefault.png'
+import graphicdesign from '../assets/Homepage/tempdefault.png'
+import keyboards from '../assets/Homepage/tempdefault.png'
+import nametitle from '../assets/Homepage/streamText.gif'
+import about1 from '../assets/About/DSCF0789.jpg'
+import about2 from '../assets/About/DSCF1339.jpg'
+import about3 from '../assets/About/DSCF1474.jpg'
+import blendifyposter from '../assets/Blendify/Blendify-Poster-Board.png'
+import blendifyblack from '../assets/Blendify/Blendify Logo.png'
+import blendifypurple from '../assets/Blendify/Blendify Logo Purple.png'
+// import calpolyfront from '../assets/GraphicDesign/FRONT.png'
+// import calpolyback from '../assets/GraphicDesign/CPR.png'
+// import calpolyhoodfront from '../assets/GraphicDesign/cal poly hoodie front mockup.png'
+// import calpolyhoodback from '../assets/GraphicDesign/cal poly hoodie back mockup cpr.png'
+// import calpolyshirt from '../assets/GraphicDesign/cal poly shirt mock up.png'
+// import flowerdesign from '../assets/GraphicDesign/flower wide.png'
+// import nsufront from '../assets/GraphicDesign/nsu front logo.png'
+// import nsuback from '../assets/GraphicDesign/nsu back.png'
+// import nsushirt from '../assets/GraphicDesign/nsu tshirt mock up.png'
+// import serrafront from '../assets/GraphicDesign/JUNGLE GAME DESIGN FRONT v2.png'
+// import serraback from '../assets/GraphicDesign/JUNGLE GAME DESIGN BACK V2.png'
+// import serraside from '../assets/GraphicDesign/JUNGLE GAME DESIGN ON SHIRTS v2.png'
+// import symposiumfront from '../assets/GraphicDesign/LMU APAM T shirt Front.png'
+// import symposiumback from '../assets/GraphicDesign/LMU APAM T shirt Back.png'
+// import symposiumside from '../assets/GraphicDesign/LMU APAM T shirt Mockup.png'
+
 
 export const images = {
     tattoovertical: tattoovertical,
@@ -38,5 +54,20 @@ export const images = {
     blendifyposter: blendifyposter,
     blendifyblack: blendifyblack,
     blendifypurple: blendifypurple,
+    // calpolyfront : calpolyfront,
+    // calpolyback : calpolyback,
+    // calpolyhoodfront : calpolyhoodfront,
+    // calpolyhoodback : calpolyhoodback,
+    // calpolyshirt : calpolyshirt,
+    // flowerdesign : flowerdesign,
+    // nsufront : nsufront,
+    // nsuback : nsuback,
+    // nsushirt : nsushirt,
+    // serrafront : serrafront,
+    // serraback : serraback,
+    // serraside : serraside,
+    // symposiumfront : symposiumfront,
+    // symposiumback : symposiumback,
+    // symposiumside : symposiumside,
   };
   
