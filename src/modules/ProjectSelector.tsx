916fd119f0813f5f87278622ec102382fe85cac0
Sebastian "Seabass" Cruz <haloguy1175@gmail.com>
2026-07-23 22:32:08 -0700

    adjust extra scroll space on homepage

--- src/modules/ProjectSelector.tsx
+++ src/modules/ProjectSelector.tsx
@@ -1,50 +1,58 @@
-import { useState } from 'react';
-import './ProjectSelector.css';
-import { Stack, Image, Group, Text} from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
-import ProjectList from './ProjectList';
-import {videos} from '../helpers/videos.ts'
-import {images} from '../helpers/images.ts'
+import { useState } from "react";
+import "./ProjectSelector.css";
+import { Stack, Image, Group, Text } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import ProjectList from "./ProjectList";
+import { videos } from "../helpers/videos.ts";
+import { images } from "../helpers/images.ts";
 
-import { theme } from '../theme'; 
+import { theme } from "../theme";
 
 function ProjectSelector() {
   const [videoSrc, setVideoSrc] = useState(videos.loadingvideo);
-  
-  const isMobile = useMediaQuery(`(max-width: ${theme?.breakpoints?.lg})`);
-  const text_breakpoint_right = isMobile ? '0px' : '12%';
-  const text_breakpoint_left = isMobile ? '10px' : '20px';
-  const breakpoint_top = isMobile? '70px' : '50px';
-  const navlink_title_breakpoint = isMobile ? 'clamp(3rem, 7vw, 5rem)' : 'clamp(3rem, 3vw, 5rem)';
 
-  const tv_breakpoint_width = isMobile ? '85vw' : '40vw';
-  const tv_background_transform = isMobile? 'translate(0%, -32%)' : 'translate(-3%, -32%)'
-  const tv_mask_transform = isMobile? 'translate(0%, -31%)' : 'translate(-3%, -32%)'
-  const tv_video_transform = isMobile? 'translate(-7%, -44%)' : 'translate(-11%, -46.5%)'
+  const isMobile = useMediaQuery(`(max-width: ${theme?.breakpoints?.lg})`);
+  const text_breakpoint_right = isMobile ? "0px" : "12%";
+  const text_breakpoint_left = isMobile ? "10px" : "20px";
+  const breakpoint_top = isMobile ? "70px" : "50px";
+  const navlink_title_breakpoint = isMobile
+    ? "clamp(3rem, 7vw, 5rem)"
+    : "clamp(3rem, 3vw, 5rem)";
+
+  const tv_breakpoint_width = isMobile ? "90vw" : "40vw";
 
   const handleVideoChange = (newVideoSrc: string) => {
     setVideoSrc(newVideoSrc);
   };
 
   return (
-    <Stack 
-      align= {isMobile ? 'stretch' : 'flex-start' }
-      justify='flex-start' 
-      style={{ 
-        paddingRight: text_breakpoint_right, 
-        paddingLeft: text_breakpoint_left, 
-        paddingTop: breakpoint_top}}>
-      <Group justify='flex-start'>
+    <Stack
+      align={isMobile ? "stretch" : "flex-start"}
+      justify="flex-start"
+      style={{
+        paddingRight: text_breakpoint_right,
+        paddingLeft: text_breakpoint_left,
+        paddingTop: breakpoint_top,
+      }}
+    >
+      <Group justify="flex-start">
         <div>
-          <Text className="projectTitle" style={{fontSize: navlink_title_breakpoint}}>
+          <Text
+            className="projectTitle"
+            style={{ fontSize: navlink_title_breakpoint }}
+          >
             Projects
           </Text>
           <ProjectList
             links={[
-              ['keyboardwarrior', 'Keyboard Warrior', videos.keyboardwarriorvideo],
-              ['quickcast', 'Quickcast', videos.quickcastvideo],
-              ['blendify', 'Blendify', videos.blendifyvideo],
-              ['sussyscript', 'SussyScript', videos.sussyscriptvideo],
+              [
+                "keyboardwarrior",
+                "Keyboard Warrior",
+                videos.keyboardwarriorvideo,
+              ],
+              ["quickcast", "Quickcast", videos.quickcastvideo],
+              ["blendify", "Blendify", videos.blendifyvideo],
+              ["sussyscript", "SussyScript", videos.sussyscriptvideo],
             ]}
             setVideoSrc={handleVideoChange} // Pass the function to handle image change
             style={{
@@ -53,16 +61,18 @@ function ProjectSelector() {
           />
         </div>
         <div>
-          <Text className="projectTitle" style={{fontSize: navlink_title_breakpoint}}> 
+          <Text
+            className="projectTitle"
+            style={{ fontSize: navlink_title_breakpoint }}
+          >
             Passions
           </Text>
           <ProjectList
             links={[
-              ['photography', 'Photography', videos.photographyvideo],
-              ['nishikigoi', 'Nishikigoi', videos.nishikigoivideo],
-              ['graphicdesign', 'Graphic Design', videos.graphicdesignvideo],
-              ['keyboards', 'Keyboards', videos.keyboardvideo],
-
+              ["photography", "Photography", videos.photographyvideo],
+              ["nishikigoi", "Nishikigoi", videos.nishikigoivideo],
+              ["graphicdesign", "Graphic Design", videos.graphicdesignvideo],
+              ["keyboards", "Keyboards", videos.keyboardvideo],
             ]}
             setVideoSrc={handleVideoChange} // Pass the function to handle image change
             style={{
@@ -71,78 +81,63 @@ function ProjectSelector() {
           />
         </div>
       </Group>
-      <div 
-        style={{ 
-          position: 'relative',
-          overflow: 'hidden', 
-          height: 'auto', 
+      <div
+        style={{
+          display: "grid",
           width: tv_breakpoint_width,
-          aspectRatio: '9 / 16',
+          justifyItems: "center",
         }}
       >
-  <Image 
-    src={images.tvbackground} 
-    alt="tv background" 
-    fit="contain"
-    style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%', 
-      height: '100%',
-      transform: tv_background_transform,
-    }} 
-    className="background"
-  />
+        {/* Background */}
+        <Image
+          src={images.tvbackground}
+          fit="contain"
+          style={{
+            gridArea: "1 / 1",
+            width: "100%",
+          }}
+        />
 
-  <div
-    style={{
-      position: 'absolute',
-      top: '15%',
-      left: '17.5%',
-      width: '65%',
-      height: '70%',
-      overflow: 'hidden',
-      transform: tv_video_transform,
-    }}
-  >
-    <video
-      src={videoSrc}
-      autoPlay
-      muted
-      loop
-      playsInline
-      style={{
-        width: '142%',
-        height: '100%',
-        position: 'absolute',
-        top: 0,
-        left: '-20%',
-      }}
-      className="background-image"
-    />
-  </div>
+        {/* Video */}
+        <div
+          style={{
+            gridArea: "1 / 1",
+            alignSelf: "center",
+            justifySelf: "center",
 
-  <Image 
-    src={images.tvmask} 
-    alt="tv mask" 
-    fit="contain" 
-    style={{ 
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%', 
-      height: '100%',
-      transform: tv_mask_transform,
-    }} 
-    className="background"
-  />
-</div>
+            width: "90%",
+            height: "75%",
 
-<div className="text">
-  ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧
-</div>
+            overflow: "hidden",
+          }}
+        >
+          <video
+            src={videoSrc}
+            autoPlay
+            muted
+            loop
+            playsInline
+            style={{
+              width: "100%",
+              height: "100%",
+              transform: "translate(-30px, -10px)",
+            }}
+          />
+        </div>
 
+        {/* Foreground */}
+        <Image
+          src={images.tvmask}
+          fit="contain"
+          style={{
+            gridArea: "1 / 1",
+            width: "100%",
+            transform: "translate(0px, 0px)",
+          }}
+        />
+      </div>
+
+      <div className="text">ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧</div>
     </Stack>
   );
 }
